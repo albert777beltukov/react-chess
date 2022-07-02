9bd73d8e997fe95f35fcd4109537ad7e382d2bc4
Your Name <you@example.com>
2022-07-02 19:03:34 +0400

    Add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import "./App.scss";
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
 
 const App = () => {
-  return (
-    <div>
 
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
+  return (
+    <div className='app'>
+    <BoardComponent
+      board={board}
+      setBoard={setBoard}
+    />
     </div>
+    
   );
 };
 
